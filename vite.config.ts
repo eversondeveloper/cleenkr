dd5a84adbf2f7ca25d36c7726c978cb7b8b02bcc
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2026-05-04 20:45:36 -0300

    refactor: cria tsconfig.json e prepara terreno para implementação do types

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Como o package.json usa "type": "module", precisamos deste pequeno truque 
+// para o Node conseguir entender o __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
